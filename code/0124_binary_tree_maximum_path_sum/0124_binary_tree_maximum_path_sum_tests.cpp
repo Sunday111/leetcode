bbab0e2586ac326693004b2e5cd2132a52be2571
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2025-04-22 19:30:25 +0000

    LeetcodeBinaryTree utility

--- code/0124_binary_tree_maximum_path_sum/0124_binary_tree_maximum_path_sum_tests.cpp
+++ code/0124_binary_tree_maximum_path_sum/0124_binary_tree_maximum_path_sum_tests.cpp
@@ -1,96 +1,10 @@
 #include <cassert>
-#include <deque>
-#include <sstream>
 
 #include "0124_binary_tree_maximum_path_sum.hpp"
 #include "gtest/gtest.h"
+#include "test_utility.hpp"
 
-struct Tree
-{
-    std::deque<TreeNode> nodes;
-    std::vector<uint64_t> free_nodes;
-    TreeNode* root = nullptr;
-
-    [[nodiscard]] static Tree FromArray(std::span<std::optional<int>> values)
-    {
-        Tree tree;
-
-        assert(!values.empty());
-        assert(values.front().has_value());
-
-        tree.root = &tree.nodes.emplace_back(TreeNode{*values.front()});
-        std::vector<std::pair<TreeNode*, size_t>> queue{
-            {tree.root, 0},
-        };
-
-        while (!queue.empty())
-        {
-            auto [node, iroot] = queue.back();
-            queue.pop_back();
-
-            if (size_t ileft = iroot * 2 + 1; ileft < values.size())
-            {
-                if (const auto& opt = values[ileft])
-                {
-                    node->left = &tree.nodes.emplace_back(TreeNode{*opt});
-                    queue.push_back({node->left, ileft});
-                }
-            }
-
-            if (size_t iright = iroot * 2 + 2; iright < values.size())
-            {
-                if (const auto& opt = values[iright])
-                {
-                    node->right = &tree.nodes.emplace_back(TreeNode{*opt});
-                    queue.push_back({node->right, iright});
-                }
-            }
-        }
-
-        return tree;
-    }
-
-    [[nodiscard]] static std::vector<std::optional<int>> StringToArray(
-        std::string_view str)
-    {
-        std::stringstream stream(std::string{str});
-        std::vector<std::optional<int>> r;
-
-        while (!stream.eof())
-        {
-            if (int c = stream.peek(); std::isdigit(c) || c == '-')
-            {
-                int value{};
-                stream >> value;
-                r.push_back(value);
-            }
-            else
-            {
-                int c0 = stream.get();
-                int c1 = stream.get();
-                int c2 = stream.get();
-                int c3 = stream.get();
-
-                assert(c0 == 'n');
-                assert(c1 == 'u');
-                assert(c2 == 'l');
-                assert(c3 == 'l');
-
-                r.push_back(std::nullopt);
-            }
-
-            if (int p = stream.peek(); p == ',') stream.get();
-        }
-
-        return r;
-    }
-
-    [[nodiscard]] static Tree FromString(std::string_view str)
-    {
-        auto array = StringToArray(str);
-        return FromArray(array);
-    }
-};
+using Tree = LeetCodeBinaryTree<TreeNode>;
 
 TEST(t0124_binary_tree_maximum_path_sum, test_1)
 {
--- test_utility/test_utility.hpp
+++ test_utility/test_utility.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <cassert>
+#include <optional>
 #include <queue>
+#include <span>
+#include <sstream>
 
 /**
  * Definition for a binary tree node.
@@ -11,10 +15,10 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-template <typename Node>
-bool CompareBinaryTrees(Node* root_a, Node* root_b)
+template <typename BinaryTreeNode>
+bool CompareBinaryTrees(BinaryTreeNode* root_a, BinaryTreeNode* root_b)
 {
-    std::queue<std::pair<Node*, Node*>> q{{root_a, root_b}};
+    std::queue<std::pair<BinaryTreeNode*, BinaryTreeNode*>> q{{root_a, root_b}};
 
     while (!q.empty())
     {
@@ -31,3 +35,93 @@ bool CompareBinaryTrees(Node* root_a, Node* root_b)
 
     return true;
 }
+
+template <typename BinaryTreeNode>
+struct LeetCodeBinaryTree
+{
+    std::deque<BinaryTreeNode> nodes;
+    std::vector<uint64_t> free_nodes;
+    BinaryTreeNode* root = nullptr;
+
+    [[nodiscard]] static LeetCodeBinaryTree FromArray(
+        std::span<std::optional<int>> values)
+    {
+        LeetCodeBinaryTree<BinaryTreeNode> tree;
+
+        assert(!values.empty());
+        assert(values.front().has_value());
+
+        tree.root = &tree.nodes.emplace_back(BinaryTreeNode{*values.front()});
+        std::vector<std::pair<BinaryTreeNode*, size_t>> queue{
+            {tree.root, 0},
+        };
+
+        while (!queue.empty())
+        {
+            auto [node, iroot] = queue.back();
+            queue.pop_back();
+
+            if (size_t ileft = iroot * 2 + 1; ileft < values.size())
+            {
+                if (const auto& opt = values[ileft])
+                {
+                    node->left = &tree.nodes.emplace_back(BinaryTreeNode{*opt});
+                    queue.push_back({node->left, ileft});
+                }
+            }
+
+            if (size_t iright = iroot * 2 + 2; iright < values.size())
+            {
+                if (const auto& opt = values[iright])
+                {
+                    node->right =
+                        &tree.nodes.emplace_back(BinaryTreeNode{*opt});
+                    queue.push_back({node->right, iright});
+                }
+            }
+        }
+
+        return tree;
+    }
+
+    [[nodiscard]] static std::vector<std::optional<int>> StringToArray(
+        std::string_view str)
+    {
+        std::stringstream stream(std::string{str});
+        std::vector<std::optional<int>> r;
+
+        while (!stream.eof())
+        {
+            if (int c = stream.peek(); std::isdigit(c) || c == '-')
+            {
+                int value{};
+                stream >> value;
+                r.push_back(value);
+            }
+            else
+            {
+                int c0 = stream.get();
+                int c1 = stream.get();
+                int c2 = stream.get();
+                int c3 = stream.get();
+
+                assert(c0 == 'n');
+                assert(c1 == 'u');
+                assert(c2 == 'l');
+                assert(c3 == 'l');
+
+                r.push_back(std::nullopt);
+            }
+
+            if (int p = stream.peek(); p == ',') stream.get();
+        }
+
+        return r;
+    }
+
+    [[nodiscard]] static LeetCodeBinaryTree FromString(std::string_view str)
+    {
+        auto array = StringToArray(str);
+        return FromArray(array);
+    }
+};
