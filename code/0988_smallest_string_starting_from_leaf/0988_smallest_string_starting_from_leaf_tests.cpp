321dcee0679e5cbc09c153c3d54b5bca35132f69
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2025-05-29 01:16:07 +0100

    Fix

--- code/0988_smallest_string_starting_from_leaf/0988_smallest_string_starting_from_leaf_tests.cpp
+++ code/0988_smallest_string_starting_from_leaf/0988_smallest_string_starting_from_leaf_tests.cpp
@@ -13,16 +13,10 @@ TEST(t0988_smallest_string_starting_from_leaf, test_1)
 TEST(t0988_smallest_string_starting_from_leaf, test_2)
 {
     Solution s{};
-    auto tree = Tree::FromString("4,0,0,1");
-    ASSERT_EQ(s.smallestFromLeaf(tree.root), "bae");
-}
-TEST(t0988_smallest_string_starting_from_leaf, test_3)
-{
-    Solution s{};
     auto tree = Tree::FromString("25,1,3,1,3,0,2");
     ASSERT_EQ(s.smallestFromLeaf(tree.root), "adz");
 }
-TEST(t0988_smallest_string_starting_from_leaf, test_4)
+TEST(t0988_smallest_string_starting_from_leaf, test_3)
 {
     Solution s{};
     auto tree = Tree::FromString("25,0,null,1,null,0,0,1,null,null,null,0");
