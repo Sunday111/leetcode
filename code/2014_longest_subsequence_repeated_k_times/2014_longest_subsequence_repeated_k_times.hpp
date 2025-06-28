22a0bb7de9955829b98a179ac99e6b43ec3ba62c
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2025-06-28 11:58:35 +0100

    2014. Longest Subsequence Repeated k Times

--- code/2014_longest_subsequence_repeated_k_times/2014_longest_subsequence_repeated_k_times.hpp
+++ code/2014_longest_subsequence_repeated_k_times/2014_longest_subsequence_repeated_k_times.hpp
@@ -1,10 +1,7 @@
 #pragma once
 
-#include <algorithm>
 #include <array>
 #include <cassert>
-#include <optional>
-#include <span>
 #include <string>
 
 using u8 = uint8_t;
@@ -22,166 +19,117 @@ using i64 = int64_t;
 #define ATTR inline
 #endif
 
-template <typename T, uint16_t C>
-class SmallArray
-{
-public:
-    constexpr SmallArray() noexcept = default;
-    constexpr SmallArray(const SmallArray&) noexcept = default;
-    constexpr SmallArray& operator=(const SmallArray&) noexcept = default;
-    static constexpr auto capacity = C;
-    [[nodiscard]] ATTR constexpr auto view() noexcept
-    {
-        return std::span{data_}.first(size_);
-    }
-    [[nodiscard]] ATTR constexpr auto view() const noexcept
-    {
-        return std::span{data_}.first(size_);
-    }
-
-    constexpr T& push_back(const T& v) noexcept { return (data_[size_++] = v); }
-    [[nodiscard]] ATTR constexpr bool isFull() const noexcept
-    {
-        return size_ == capacity;
-    }
-
-    [[nodiscard]] ATTR constexpr std::optional<T> optLast() const noexcept
-    {
-        if (size_) return std::optional<T>{std::in_place, data_[size_ - 1]};
-        return std::nullopt;
-    }
-
-    [[nodiscard]] ATTR constexpr const T& operator[](u16 idx) const noexcept
-    {
-        return data_[idx];
-    }
-
-    [[nodiscard]] ATTR constexpr u16 size() const noexcept { return size_; }
-    void pop_back() { --size_; }
-
-private:
-    std::array<T, capacity> data_{};
-    uint16_t size_ = 0;
-};
-
 class Solution
 {
 public:
-    struct CharInfo
-    {
-        SmallArray<u16, 2000> indices;
-        [[nodiscard]] u16 num() const noexcept { return indices.size(); }
-    };
+    [[nodiscard]] ATTR static constexpr u8 charToIdx(char c) noexcept { return (c - 'a') & 0xFF; }
+    [[nodiscard]] ATTR static constexpr char idxToChar(u8 c) noexcept { return static_cast<char>(c + 'a'); }
 
-    [[nodiscard]] ATTR static constexpr u8 charToIdx(char c) noexcept
+    [[nodiscard]] ATTR static constexpr std::string longestSubsequenceRepeatedK(std::string& s, u16 k) noexcept
     {
-        return (c - 'a') & 0xFF;
-    }
-    [[nodiscard]] ATTR static constexpr char idxToChar(u8 c) noexcept
-    {
-        return static_cast<char>(c + 'a');
-    }
-
-    [[nodiscard]] ATTR bool test_current() const noexcept
-    {
-        std::array<char, 7> chars{};
-
-        for (u8 i = 0; i != current.size(); ++i) chars[i] = s[current[i]];
-
-        size_t pos = 0;
-        for (u16 ki = 0; ki != k; ++ki)
+        // prepare lookup info
+        std::array<u16, 26> freq{};
+        const std::string chars = [&]
         {
-            for (u8 j = 0; j != current.size(); ++j)
+            for (char c : s) freq[charToIdx(c)]++;
+
+            // remove chars with freq < k
+            u16 n = 0;
+            for (char c : s)
             {
-                char c = chars[j];
-                pos = s.find(c, pos);
-                if (pos == s.npos) return false;
-                ++pos;
+                u8 ci = charToIdx(c);
+                if (freq[ci] >= k) s[n++] = c;
             }
-        }
+            s.resize(n);
 
-        return true;
-    }
-
-    void dfs(u16 start)
-    {
-        for (u8 i = 0; i != sorted_info.size(); ++i)
-        {
-            auto& ci = *sorted_info[i];
-
-            const u16* ci_idx = nullptr;
-            for (u16 j = 0; j != ci.indices.size(); ++j)
+            // unique chars
+            std::string chars;
+            u8 ci = freq.size();
+            do
             {
-                if (const u16& v = ci.indices[j]; v >= start)
+                --ci;
+                if (freq[ci] >= k)
                 {
-                    ci_idx = &v;
-                    break;
+                    chars.push_back(idxToChar(ci));
+                }
+            } while (ci != 0);
+
+            return chars;
+        }();
+
+        std::string current, best;
+
+        auto push_update = [&](u8 i, char push_char)
+        {
+            const u8 ci = charToIdx(push_char);
+            if (freq[ci] < k) return false;
+
+            current.resize(i);
+            current.push_back(push_char);
+
+            // Find current subsequence k times in s
+            size_t end = 0;
+            for (u16 ki = 0; ki != k; ++ki)
+            {
+                for (char c : current)
+                {
+                    end = s.find(c, end);
+                    if (end >= s.size()) return false;
+                    ++end;
                 }
             }
 
-            if (ci_idx)
+            // only update best if validation above succeeded
+            if (current.size() > best.size()) best = current;
+
+            freq[ci] -= k;
+            return true;
+        };
+
+        for (char c0 : chars)
+        {
+            if (!push_update(0, c0)) continue;
+
+            for (char c1 : chars)
             {
-                current.push_back(*ci_idx);
-                if (test_current())
+                if (!push_update(1, c1)) continue;
+
+                for (char c2 : chars)
                 {
-                    if (current.size() > best.size())
+                    if (!push_update(2, c2)) continue;
+
+                    for (char c3 : chars)
                     {
-                        best = current;
-                        if (current.isFull()) throw 42;  // NOLINT
+                        if (!push_update(3, c3)) continue;
+
+                        for (char c4 : chars)
+                        {
+                            if (!push_update(4, c4)) continue;
+
+                            for (char c5 : chars)
+                            {
+                                if (!push_update(5, c5)) continue;
+
+                                for (char c6 : chars)
+                                {
+                                    if (push_update(6, c6))
+                                    {
+                                        return best;
+                                    }
+                                }
+                                freq[charToIdx(c5)] += k;
+                            }
+                            freq[charToIdx(c4)] += k;
+                        }
+                        freq[charToIdx(c3)] += k;
                     }
-
-                    // Try this character
-                    dfs(*ci_idx + 1);
+                    freq[charToIdx(c2)] += k;
                 }
-                current.pop_back();
+                freq[charToIdx(c1)] += k;
             }
+            freq[charToIdx(c0)] += k;
         }
+
+        return best;
     }
-
-    [[nodiscard]] std::string longestSubsequenceRepeatedK(
-        const std::string& s_,
-        u16 k_) noexcept
-    {
-        s = s_;
-        k = k_;
-        current = {};
-        best = {};
-
-        std::array<CharInfo, 26> info{};
-        for (u16 i = 0; i != s.size(); ++i)
-        {
-            info[charToIdx(s[i])].indices.push_back(i);
-        }
-
-        std::array<const CharInfo*, 26> pointers;  // NOLINT
-        {
-            for (size_t i = 0; i != info.size(); ++i)
-            {
-                pointers[i] = &info[i];
-            }
-            std::ranges::reverse(pointers);
-            auto [ret, last] = std::ranges::remove_if(
-                pointers,
-                [&](const CharInfo* ci) { return ci->num() < k; });
-            sorted_info = std::span{pointers.begin(), ret};
-        }
-
-        try
-        {
-            dfs(0);
-        }
-        catch (...)
-        {
-        }
-
-        std::string result;
-        for (u16 index : best.view()) result.push_back(s[index]);
-
-        return result;
-    }
-
-    std::span<const CharInfo* const> sorted_info;  // NOLINT
-    SmallArray<u16, 7> current{}, best{};
-    std::string_view s;
-    u16 k;
 };
--- code/2014_longest_subsequence_repeated_k_times/2014_longest_subsequence_repeated_k_times_tests.cpp
+++ code/2014_longest_subsequence_repeated_k_times/2014_longest_subsequence_repeated_k_times_tests.cpp
@@ -3,29 +3,30 @@
 
 TEST(t2014_longest_subsequence_repeated_k_times, test_1)
 {
-    ASSERT_EQ(Solution{}.longestSubsequenceRepeatedK("letsleetcode", 2), "let");
+    std::string s = "letsleetcode";
+    ASSERT_EQ(Solution{}.longestSubsequenceRepeatedK(s, 2), "let");
 }
 
 TEST(t2014_longest_subsequence_repeated_k_times, test_2)
 {
-    ASSERT_EQ(Solution{}.longestSubsequenceRepeatedK("bb", 2), "b");
+    std::string s = "bb";
+    ASSERT_EQ(Solution{}.longestSubsequenceRepeatedK(s, 2), "b");
 }
 
 TEST(t2014_longest_subsequence_repeated_k_times, test_3)
 {
-    ASSERT_EQ(Solution{}.longestSubsequenceRepeatedK("ab", 2), "");
+    std::string s = "ab";
+    ASSERT_EQ(Solution{}.longestSubsequenceRepeatedK(s, 2), "");
 }
 
 TEST(t2014_longest_subsequence_repeated_k_times, test_4)
 {
-    ASSERT_EQ(
-        Solution{}.longestSubsequenceRepeatedK("nhsbbeonhsbbeonhsbbeo", 3),
-        "nhsbbeo");
+    std::string s = "nhsbbeonhsbbeonhsbbeo";
+    ASSERT_EQ(Solution{}.longestSubsequenceRepeatedK(s, 3), "nhsbbeo");
 }
 
 TEST(t2014_longest_subsequence_repeated_k_times, test_5)
 {
-    ASSERT_EQ(
-        Solution{}.longestSubsequenceRepeatedK("gbjbjigjbji", 2),
-        "gjbji");
+    std::string s = "gbjbjigjbji";
+    ASSERT_EQ(Solution{}.longestSubsequenceRepeatedK(s, 2), "gjbji");
 }
