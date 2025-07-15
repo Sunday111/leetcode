8afe122c2e632fcd6711f52294823fd874d2c0dc
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2025-07-15 22:59:29 +0100

    268. Missing Number

---
+++ code/0268_missing_number/0268_missing_number.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <numeric>
+#include <vector>
+
+class Solution
+{
+public:
+    [[nodiscard]] int missingNumber(std::vector<int>& nums) noexcept
+    {
+        const int n = static_cast<int>(nums.size());
+        return ((1 + n) * n) / 2 - std::accumulate(nums.begin(), nums.end(), 0);
+    }
+};
