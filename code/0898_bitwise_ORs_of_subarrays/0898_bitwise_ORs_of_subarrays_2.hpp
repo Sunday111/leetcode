ac7dd720e077f932da5ad27cda51ded0ab21aa74
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2025-07-31 23:54:25 +0100

    898. Bitwise ORs of Subarrays

---
+++ code/0898_bitwise_ORs_of_subarrays/0898_bitwise_ORs_of_subarrays_2.hpp
@@ -0,0 +1,68 @@
+#include <array>
+#include <cstdint>
+#include <vector>
+
+using u32 = uint32_t;
+using i32 = int32_t;
+
+#ifdef __GNUC__
+#define FORCE_INLINE inline __attribute__((always_inline))
+#else
+#define FORCE_INLINE inline
+#endif
+
+class HashSet
+{
+public:
+    using ValueType = i32;
+    static constexpr ValueType kInvalidValue = -1;
+    static constexpr std::hash<ValueType> kHasher{};
+    static constexpr u32 mask_ = (~u32{0}) >> (32 - 21);
+
+    FORCE_INLINE void clear() noexcept { values_.fill(kInvalidValue); }
+
+    FORCE_INLINE bool add(ValueType key) noexcept
+    {
+        u32 index = kHasher(key) & mask_;
+        while (values_[index] != kInvalidValue && values_[index] != key)
+        {
+            ++index;
+            index &= mask_;
+        }
+        return std::exchange(values_[index], key) != key;
+    }
+
+private:
+    std::array<ValueType, 1 << 21> values_;
+};
+
+class Solution
+{
+public:
+    [[nodiscard]] static constexpr size_t subarrayBitwiseORs(
+        std::vector<i32>& a) noexcept
+    {
+        const u32 n = static_cast<u32>(a.size());
+
+        static HashSet hs;
+        hs.clear();
+
+        u32 num_results = 0;
+
+        for (u32 i = 0; i != n; ++i)
+        {
+            const i32 x = a[i];
+            i32 skip = 0, take = x;
+            num_results += hs.add(take);
+            for (u32 j = i; j--;)
+            {
+                skip |= a[j];
+                take = skip | x;
+                if (skip == take) break;
+                num_results += hs.add(take);
+            }
+        }
+
+        return num_results;
+    }
+};
---
+++ code/0898_bitwise_ORs_of_subarrays/0898_bitwise_ORs_of_subarrays_tests.cpp
@@ -0,0 +1,7 @@
+// #include "0898_bitwise_ORs_of_subarrays.hpp"
+// #include "gtest/gtest.h"
+
+// TEST(t0898_bitwise_ORs_of_subarrays, test_1)
+// {
+//     Solution s{};
+// }
