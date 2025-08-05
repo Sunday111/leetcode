85bf558e0e26e55da1c838557231e30531c26238
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2025-08-05 01:08:02 +0100

    3479. Fruits Into Baskets III

--- code/3479_fruits_into_baskets_3/3479_fruits_into_baskets_3.hpp
+++ code/3479_fruits_into_baskets_3/3479_fruits_into_baskets_3.hpp
@@ -4,6 +4,9 @@
 #include <span>
 #include <vector>
 
+#define FORCE_INLINE inline __attribute__((always_inline))
+#define HOT_PATH __attribute__((hot))
+
 using u8 = uint8_t;
 using u16 = uint16_t;
 using u32 = uint32_t;
@@ -17,14 +20,14 @@ using i64 = int64_t;
 class SegmentTree
 {
 public:
-    void Build(std::span<const int> data_)
+    FORCE_INLINE constexpr void Build(std::span<const int> data_) noexcept
     {
         data = data_;
         n = static_cast<u32>(data.size());
         BuildR(0, 0, n - 1);
     }
 
-    void BuildR(u32 node, u32 l, u32 r)
+    constexpr void BuildR(u32 node, u32 l, u32 r) noexcept
     {
         if (l == r)
         {
@@ -38,93 +41,103 @@ public:
         nodes[node] = std::max(nodes[2 * node + 1], nodes[2 * node + 2]);
     }
 
-    void Update(u32 idx, int val) { UpdateR(idx, val, 0, 0, n - 1); }
-
-    void UpdateR(u32 idx, int val, u32 node, u32 l, u32 r)
+    FORCE_INLINE constexpr void Update(u32 idx, int val) noexcept
     {
-        if (l == r)
+        // Leaf node index in the segment tree array
+        u32 node = 0, l = 0, r = n - 1;
+
+        // Find the leaf corresponding to idx iteratively
+        while (l != r)
         {
-            nodes[node] = val;
-            return;
+            u32 mid = (l + r) / 2;
+            if (idx <= mid)
+            {
+                node = 2 * node + 1;
+                r = mid;
+            }
+            else
+            {
+                node = 2 * node + 2;
+                l = mid + 1;
+            }
         }
 
-        u32 mid = (l + r) / 2;
-        if (idx <= mid)
-        {
-            UpdateR(idx, val, 2 * node + 1, l, mid);
-        }
-        else
-        {
-            UpdateR(idx, val, 2 * node + 2, mid + 1, r);
-        }
+        // Update leaf value
+        nodes[node] = val;
 
-        nodes[node] = std::max(nodes[2 * node + 1], nodes[2 * node + 2]);
+        // Move up and update parents
+        while (node != 0)
+        {
+            node = (node - 1) / 2;
+            nodes[node] = std::max(nodes[2 * node + 1], nodes[2 * node + 2]);
+        }
     }
 
     template <typename Cmp>
-    u32 UpdateFirst(int search_val, const Cmp& cmp, int replace_val)
+    [[nodiscard]] FORCE_INLINE HOT_PATH constexpr u32
+    UpdateFirst(int val, const Cmp& cmp, int replace_val) noexcept
     {
-        return UpdateFirst(search_val, cmp, replace_val, 0, 0, n - 1);
-    }
+        u32 node = 0, l = 0, r = n - 1;
 
-    template <typename Cmp>
-    u32 UpdateFirst(
-        int search_val,
-        const Cmp& cmp,
-        int replace_val,
-        u32 node,
-        u32 l,
-        u32 r)
-    {
-        if (!cmp(nodes[node], search_val)) return n;
-        if (l == r)
+        if (!cmp(nodes[node], val)) return n;  // no candidate
+
+        // Traverse down the tree
+        while (l != r)
         {
-            nodes[node] = replace_val;
-            return l;
+            u32 mid = (l + r) / 2;
+
+            if (cmp(nodes[2 * node + 1], val))
+            {
+                // Go left
+                node = 2 * node + 1;
+                r = mid;
+            }
+            else
+            {
+                // Go right
+                node = 2 * node + 2;
+                l = mid + 1;
+            }
         }
 
-        u32 mid = (l + r) / 2;
-        u32 idx =
-            UpdateFirst(search_val, cmp, replace_val, 2 * node + 1, l, mid);
-        if (idx == n)
-        {
-            idx = UpdateFirst(
-                search_val,
-                cmp,
-                replace_val,
-                2 * node + 2,
-                mid + 1,
-                r);
-        }
+        // Now at leaf node, update
+        nodes[node] = replace_val;
 
-        if (idx != n)
+        // Go back up and fix parents
+        while (node != 0)
         {
+            node = (node - 1) / 2;
             nodes[node] = std::max(nodes[2 * node + 1], nodes[2 * node + 2]);
         }
 
-        return idx;
+        return l;
     }
 
     template <typename Cmp>
-    u32 FindFirst(int val, const Cmp& cmp) const
+    [[nodiscard]] FORCE_INLINE constexpr u32 FindFirst(int val, const Cmp& cmp)
+        const noexcept
     {
-        return FindFirst(val, cmp, 0, 0, n - 1);
-    }
+        u32 node = 0, l = 0, r = n - 1;
 
-    template <typename Cmp>
-    u32 FindFirst(int val, const Cmp& cmp, u32 node, u32 l, u32 r) const
-    {
         if (!cmp(nodes[node], val)) return n;
-        if (l == r) return l;
 
-        u32 mid = (l + r) / 2;
-
-        if (u32 result = FindFirst(val, cmp, 2 * node + 1, l, mid); result != n)
+        while (l != r)
         {
-            return result;
+            u32 mid = (l + r) / 2;
+
+            if (cmp(nodes[2 * node + 1], val))
+            {
+                node = 2 * node + 1;
+                r = mid;
+            }
+            else
+            {
+                node = 2 * node + 2;
+                l = mid + 1;
+            }
         }
 
-        return FindFirst(val, cmp, 2 * node + 2, mid + 1, r);
+        return l;
     }
 
     u32 n = 0;
@@ -135,21 +148,18 @@ public:
 class Solution
 {
 public:
-    [[nodiscard]] static constexpr size_t numOfUnplacedFruits(
+    [[nodiscard]] static size_t numOfUnplacedFruits(
         std::vector<int>& fruits,
-        std::vector<int>& baskets)
+        std::vector<int>& baskets) noexcept
     {
         static SegmentTree st;
         st.Build(baskets);
 
-        size_t r = fruits.size();
+        size_t r = 0;
 
         for (int f : fruits)
         {
-            if (st.UpdateFirst(f, std::greater_equal{}, 0) != st.n)
-            {
-                --r;
-            }
+            r += (st.UpdateFirst(f, std::greater_equal{}, 0) == st.n);
         }
 
         return r;
