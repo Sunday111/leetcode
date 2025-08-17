6dd82917064128ce54d828c94db91967abc02b57
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2025-08-17 15:32:50 +0100

    Update

--- code/0837_new_game/0837_new_game.hpp
+++ code/0837_new_game/0837_new_game.hpp
@@ -16,22 +16,21 @@ public:
         constexpr u16 o = 10'001;
         dp[o] = 1, n += o + 1, k += o;
 
-        double sum = 1, r = 0, m = 1 / static_cast<double>(maxPts);
+        double r = 0.0;
+        float sum = 1.0, m = 1.f / static_cast<float>(maxPts);
         u16 i = o + 1, j = i - maxPts, mnk = std::min(n, k);
 
         for (; i != mnk; ++i, ++j)
         {
-            double v = m * sum;
-            dp[i] = static_cast<float>(v);
-            sum += v - static_cast<double>(dp[j]);
+            dp[i] = m * sum;
+            sum += dp[i] - dp[j];
         }
 
         for (; i != n; ++i, ++j)
         {
-            double v = m * sum;
-            dp[i] = static_cast<float>(v);
-            r += v;
-            sum -= static_cast<double>(dp[j]);
+            dp[i] = m * sum;
+            r += static_cast<double>(dp[i]);
+            sum -= dp[j];
         }
 
         return r;
