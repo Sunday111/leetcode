7364ab644081a387afe57cf855a1618e9cc0c114
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2025-09-04 20:40:34 +0100

    2575. Find the Divisibility Array of a String

---
+++ code/2575_find_the_divisibility_array_of_a_string/2575_find_the_divisibility_array_of_a_string.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+class Solution
+{
+public:
+    [[nodiscard]] static constexpr auto divisibilityArray(
+        const std::string& word,
+        const uint32_t m) noexcept
+    {
+        std::vector<int> r;
+        r.reserve(word.size());
+
+        uint64_t v = 0;
+        for (char c : word)
+        {
+            uint8_t digit = (c - '0') & 0xFF;
+            v *= 10;
+            v += digit;
+            v %= m;
+            r.push_back(v == 0);
+        }
+
+        return r;
+    }
+};
