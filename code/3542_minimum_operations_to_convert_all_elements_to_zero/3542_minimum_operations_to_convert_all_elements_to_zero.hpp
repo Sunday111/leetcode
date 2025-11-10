e75f91822b72bde3b31147464eb2d67ebfffc118
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2025-11-10 00:58:01 +0000

    3542. Minimum Operations to Convert All Elements to Zero

---
+++ code/3542_minimum_operations_to_convert_all_elements_to_zero/3542_minimum_operations_to_convert_all_elements_to_zero.hpp
@@ -0,0 +1,36 @@
+#include <vector>
+
+class Solution
+{
+public:
+    [[nodiscard]] static constexpr uint32_t minOperations(
+        std::vector<int>& nums) noexcept
+    {
+        uint32_t s = 0, r = 0;
+        for (auto v : nums)
+        {
+            if (v)
+            {
+                while (s && nums[s - 1] > v)
+                {
+                    ++r;
+                    --s;
+                }
+
+                if (!s || nums[s - 1] != v)
+                {
+                    nums[s++] = v;
+                }
+            }
+            else
+            {
+                r += s;
+                s = 0;
+            }
+        }
+
+        return r + s;
+    }
+};
+
+#include "sync_stdio.hpp"
