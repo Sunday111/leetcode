c93843a2068c514f33e1135e50a8528c1f6691ad
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2025-11-15 16:55:39 +0000

    Fix

--- code/2536_increment_submatrices_by_one/2536_increment_submatrices_by_one_tests.cpp
+++
@@ -1,7 +0,0 @@
-#include "2536_increment_submatrices_by_one.hpp"
-#include "gtest/gtest.h"
-
-TEST(t2536_increment_submatrices_by_one, test_1)
-{
-    Solution s{};
-}
--- code/3228_maximum_number_of_operations_to_move_ones_to_the_end/3228_maximum_number_of_operations_to_move_ones_to_the_end_tests.cpp
+++
@@ -1,7 +0,0 @@
-#include "3228_maximum_number_of_operations_to_move_ones_to_the_end.hpp"
-#include "gtest/gtest.h"
-
-TEST(t3228_maximum_number_of_operations_to_move_ones_to_the_end, test_1)
-{
-    Solution s{};
-}
