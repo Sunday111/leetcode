958820c20c5c83329b13e1a7d0be8f8c50c9fbcf
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2025-12-09 03:15:50 +0000

    Aoc 12/8/2025

---
+++ code/aoc_2025_8/aoc_2025_8_tests.cpp
@@ -0,0 +1,22 @@
+#include "aoc_2025_8.hpp"
+#include "gtest/gtest.h"
+
+TEST(taoc_2025_8, test_sample_1)
+{
+    ASSERT_EQ(Solution1{}.solve(kSampleInput, 10), 40);
+}
+
+TEST(taoc_2025_8, test_input_1)
+{
+    ASSERT_EQ(Solution1{}.solve(kMyInput, 1000), 123420);
+}
+
+TEST(taoc_2025_8, test_sample_2)
+{
+    ASSERT_EQ(Solution2{}.solve(kSampleInput), 25272);
+}
+
+TEST(taoc_2025_8, test_input_2)
+{
+    ASSERT_EQ(Solution2{}.solve(kMyInput), 673096646);
+}
