2f02414e1789c3361a97f749feaf800fedeb3659
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2025-12-16 11:46:40 +0000

    3562. Maximum Profit from Trading Stocks with Discounts

--- code/3562_maximum_profit_from_trading_stocks_with_discounts/3562_maximum_profit_from_trading_stocks_with_discounts.hpp
+++ code/3562_maximum_profit_from_trading_stocks_with_discounts/3562_maximum_profit_from_trading_stocks_with_discounts.hpp
@@ -1,129 +1,131 @@
-#include <bitset>
+#include <algorithm>
 #include <cassert>
 #include <span>
-#include <unordered_map>
 #include <vector>
 
+#include "force_inline.hpp"
 #include "integral_aliases.hpp"
 
-// n [1; 160]
-// size(present), size(future) = n
-// size(hierarchy) = n - 1
-// hierarchy[i] -> [boss, sub]
-// boss, sub -> [1, n]
-// budget -> [1, 160]
-
-struct Node
+template <typename T>
+FORCE_INLINE constexpr void acc_max(T& acc, T val) noexcept
 {
-    u8 degree = 0;
-    u8 offset = 0;
-    u8 parent = 0;
-};
+    acc = std::max(acc, val);
+}
 
 class Solution
 {
 public:
-    std::span<int> present, future;
-
-    std::unordered_map<u32, int> memo;
-
-    inline static Node nodes[161];
-    inline static u8 links[160];
-    inline static u8 topological_order[160];
-
-    std::bitset<256> bought;
-    std::bitset<256> visited;
-
-    u8 topological_index = 0;
-
-    void topological_sort(u8 node_id)
+    struct Node
     {
-        if (visited[node_id]) return;
+        std::vector<u8> children;
 
-        visited[node_id] = true;
-        const auto& node = nodes[node_id];
-        const u8 child_index_end = node.offset + node.degree;
-        for (u8 li = node.offset; li != child_index_end; ++li)
+        // Index is budget, value is profit
+        std::array<int, 161> regular{};
+        std::array<int, 161> discounted{};
+    };
+
+    std::span<const int> present, future;
+
+    std::vector<Node> g;
+    u8 budget = 0;
+    u8 b1 = 0;
+    // merged from children
+    std::array<int, 161 * 3> shared_buf;
+    int *regular, *discounted, *merged;
+
+    void merge_knapsack(int* dst, const int* src) noexcept
+    {
+        std::fill_n(merged, b1, 0);
+
+        for (u8 bi = 0; bi <= budget; ++bi)
         {
-            topological_sort(links[li]);
+            for (u8 bj = 0; bj + bi <= budget; ++bj)
+            {
+                acc_max(merged[bi + bj], dst[bi] + src[bj]);
+            }
         }
-        topological_order[topological_index++] = node_id;
+
+        std::copy_n(merged, b1, dst);
     }
 
-    int dfs(u8 i, u8 budget) noexcept
+    void computeProfits(u8 node_id) noexcept
     {
-        if (budget == 0 || i == 0xFF) return 0;
-
-        int* r = nullptr;
-
-        u8 employee = topological_order[i];
-        bool discount = bought[nodes[employee].parent];
+        auto& node = g[node_id];
+        for (u8 child_id : node.children)
         {
-            u32 key = 0;
-            key = (key << 8) | u32{i};
-            key = (key << 8) | u32{employee};
-            key = (key << 8) | u32{budget};
-            key = (key << 1) | u32{discount};
-            auto [it, added] = memo.try_emplace(key, 0);
-            r = &it->second;
-            if (!added) return *r;
+            computeProfits(child_id);
         }
 
-        int price = present[employee - 1] >> discount;
-        if (price <= budget)
+        std::fill_n(regular, b1, 0);
+        std::fill_n(discounted, b1, 0);
+        for (u8 child_id : node.children)
         {
-            int profit = future[employee - 1] - price;
-
-            bought[employee] = 1;
-            *r = dfs(i - 1, (budget - price) & 0xFF) + profit;
-            bought[employee] = 0;
+            merge_knapsack(regular, g[child_id].regular.data());
         }
 
-        *r = std::max(*r, dfs(i - 1, budget));
+        for (u8 child_id : node.children)
+        {
+            merge_knapsack(discounted, g[child_id].discounted.data());
+        }
 
-        return *r;
+        const int regular_price = present[node_id - 1];
+        const int discounted_price = regular_price / 2;
+        const int regular_profit = future[node_id - 1] - regular_price;
+        const int discounted_profit = future[node_id - 1] - discounted_price;
+
+        // Case 1: Take discounted stock
+        for (u8 p = discounted_price & 0xFF, b = p; b != b1; ++b)
+        {
+            acc_max(node.discounted[b], discounted[b - p] + discounted_profit);
+        }
+
+        // Case 2: Ignore discounted stock.
+        for (u8 b = 0; b != b1; ++b)
+        {
+            acc_max(node.discounted[b], regular[b]);
+        }
+
+        // Case 3: Take without discount
+        for (u8 p = regular_price & 0xFF, b = p; b != b1; ++b)
+        {
+            acc_max(node.regular[b], discounted[b - p] + regular_profit);
+        }
+
+        // Case 4: Ignore non-discounted stock
+        for (u8 b = 0; b != b1; ++b)
+        {
+            acc_max(node.regular[b], regular[b]);
+        }
     }
 
     [[nodiscard]] int maxProfit(
-        u32 n,
+        u8 n,
         std::vector<int>& present_,
         std::vector<int>& future_,
         [[maybe_unused]] std::vector<std::vector<int>>& hierarchy,
-        u8 budget) noexcept
+        u8 budget_) noexcept
     {
+        g.clear();
+        g.resize(n + 1);
+        budget = budget_;
         present = present_;
         future = future_;
+        b1 = (budget + 1) & 0xFF;
 
-        std::fill_n(nodes, n + 1, Node{});
-
-        for (auto& edge : hierarchy)
-        {
-            u8 boss = edge[0] & 0xFF;
-            u8 sub = edge[1] & 0xFF;
-            ++nodes[boss].degree;
-            nodes[sub].parent = boss;
-        }
-
-        u8 offset = 0;
-        for (u8 id = 1; id <= n; ++id)
-        {
-            auto& node = nodes[id];
-            node.offset = offset;
-            offset += node.degree;
-            node.degree = 0;
-        }
+        regular = shared_buf.data();
+        discounted = regular + b1;
+        merged = discounted + b1;
 
         for (auto& edge : hierarchy)
         {
-            u8 boss = edge[0] & 0xFF;
-            u8 sub = edge[1] & 0xFF;
-            u8 link_index = (nodes[boss].offset + nodes[boss].degree++) & 0xFF;
-            links[link_index] = sub;
+            u8 parent = edge[0] & 0xFF;
+            u8 child = edge[1] & 0xFF;
+            g[parent].children.push_back(child);
         }
 
-        topological_sort(1);
-        assert(topological_index == future.size());
+        // 1 is always the root and all nodes are reachable
+        computeProfits(1);
 
-        return dfs(--topological_index, budget);
+        return std::ranges::max(g[1].regular);
     }
 };
--- code/3562_maximum_profit_from_trading_stocks_with_discounts/3562_maximum_profit_from_trading_stocks_with_discounts_mono.hpp
+++ code/3562_maximum_profit_from_trading_stocks_with_discounts/3562_maximum_profit_from_trading_stocks_with_discounts_mono.hpp
@@ -1,11 +1,21 @@
+#include <algorithm>
 #include <bitset>
+#include <cassert>
 #include <cstdint>
+#include <ranges>
 #include <span>
 #include <unordered_map>
+#include <utility>
 #include <vector>
 
+#define FORCE_INLINE inline __attribute__((always_inline))
+#define INLINE_LAMBDA __attribute__((always_inline))
 
-
+template <typename To>
+inline static constexpr auto cast = []<typename From>(From&& v) INLINE_LAMBDA
+{
+    return static_cast<To>(std::forward<From>(v));
+};
 
 using u8 = uint8_t;
 using u16 = uint16_t;
@@ -31,111 +41,126 @@ struct Node
     u8 parent = 0;
 };
 
+template <typename T>
+FORCE_INLINE constexpr void acc_max(T& acc, T val) noexcept
+{
+    acc = std::max(acc, val);
+}
+
 class Solution
 {
 public:
-    std::span<int> present;
-    std::span<int> future;
-
-    std::unordered_map<u32, int> memo;
-
-    inline static Node nodes[161];
-    inline static u8 links[160];
-    inline static u8 topological_order[160];
-
-    std::bitset<256> bought;
-    std::bitset<256> visited;
-    std::bitset<256> dfs_stack;
-
-    u8 topological_index = 0;
-
-    void topological_sort(u8 node_id)
+    struct Node
     {
-        if (visited[node_id]) return;
+        std::vector<u8> children;
 
-        dfs_stack[node_id] = true;
-        visited[node_id] = true;
-        const auto& node = nodes[node_id];
-        const u8 child_index_end = node.offset + node.degree;
-        for (u8 li = node.offset; li != child_index_end; ++li)
+        // Index is budget, value is profit
+        std::array<int, 161> regular{};
+        std::array<int, 161> discounted{};
+    };
+
+    std::span<const int> present, future;
+
+    std::vector<Node> g;
+    u8 budget = 0;
+    u8 b1 = 0;
+    // merged from children
+    std::array<int, 161 * 3> shared_buf;
+    int *regular, *discounted, *merged;
+
+    void merge_knapsack(int* dst, const int* src) noexcept
+    {
+        std::fill_n(merged, b1, 0);
+
+        for (u8 bi = 0; bi <= budget; ++bi)
         {
-            topological_sort(links[li]);
+            for (u8 bj = 0; bj + bi <= budget; ++bj)
+            {
+                acc_max(merged[bi + bj], dst[bi] + src[bj]);
+            }
         }
-        topological_order[topological_index++] = node_id;
-        dfs_stack[node_id] = false;
+
+        std::copy_n(merged, b1, dst);
     }
 
-    int dfs(u8 i, u8 budget) noexcept
+    void computeProfits(u8 node_id) noexcept
     {
-        if (budget == 0 || i == 0xFF) return 0;
-
-        int* r = nullptr;
-
-        u8 employee = topological_order[i];
-        bool discount = bought[nodes[employee].parent];
+        auto& node = g[node_id];
+        for (u8 child_id : node.children)
         {
-            u32 key = employee;
-            key = (key << 8) | u32{budget};
-            key = (key << 1) | u32{discount};
-            auto [it, added] = memo.try_emplace(key, 0);
-            r = &it->second;
-            if (!added) return *r;
+            computeProfits(child_id);
         }
 
-        int price = present[employee - 1] >> discount;
-        if (price <= budget)
+        std::fill_n(regular, b1, 0);
+        std::fill_n(discounted, b1, 0);
+        for (u8 child_id : node.children)
         {
-            int profit = future[employee - 1] - price;
-
-            bought[employee] = 1;
-            *r = dfs(i - 1, (budget - price) & 0xFF) + profit;
-            bought[employee] = 0;
+            merge_knapsack(regular, g[child_id].regular.data());
         }
 
-        *r = std::max(*r, dfs(i - 1, budget));
+        for (u8 child_id : node.children)
+        {
+            merge_knapsack(discounted, g[child_id].discounted.data());
+        }
 
-        return *r;
+        const int regular_price = present[node_id - 1];
+        const int discounted_price = regular_price / 2;
+        const int regular_profit = future[node_id - 1] - regular_price;
+        const int discounted_profit = future[node_id - 1] - discounted_price;
+
+        // Case 1: Take discounted stock
+        for (u8 p = discounted_price & 0xFF, b = p; b <= budget; ++b)
+        {
+            acc_max(node.discounted[b], discounted[b - p] + discounted_profit);
+        }
+
+        // Case 2: Ignore discounted stock.
+        for (u8 b = 0; b <= budget; ++b)
+        {
+            acc_max(node.discounted[b], regular[b]);
+        }
+
+        // Case 3: Take without discount
+        for (u8 p = regular_price & 0xFF, b = p; b <= budget; ++b)
+        {
+            acc_max(node.regular[b], discounted[b - p] + regular_profit);
+        }
+
+        // Case 4: Ignore non-discounted stock
+        for (u8 b = 0; b <= budget; ++b)
+        {
+            acc_max(node.regular[b], regular[b]);
+        }
     }
 
     [[nodiscard]] int maxProfit(
-        u32 n,
+        u8 n,
         std::vector<int>& present_,
         std::vector<int>& future_,
         [[maybe_unused]] std::vector<std::vector<int>>& hierarchy,
-        u8 budget) noexcept
+        u8 budget_) noexcept
     {
+        g.clear();
+        g.resize(n + 1);
+        budget = budget_;
         present = present_;
         future = future_;
+        b1 = (budget + 1) & 0xFF;
 
-        std::fill_n(nodes, n + 1, Node{});
-
-        for (auto& edge : hierarchy)
-        {
-            u8 boss = edge[0] & 0xFF;
-            u8 sub = edge[1] & 0xFF;
-            ++nodes[boss].degree;
-            nodes[sub].parent = boss;
-        }
-
-        u8 offset = 0;
-        for (u8 id = 1; id <= n; ++id)
-        {
-            auto& node = nodes[id];
-            node.offset = offset;
-            offset += node.degree;
-            node.degree = 0;
-        }
+        regular = shared_buf.data();
+        discounted = regular + b1;
+        merged = discounted + b1;
 
         for (auto& edge : hierarchy)
         {
-            u8 boss = edge[0] & 0xFF;
-            u8 sub = edge[1] & 0xFF;
-            u8 link_index = (nodes[boss].offset + nodes[boss].degree++) & 0xFF;
-            links[link_index] = sub;
+            u8 parent = edge[0] & 0xFF;
+            u8 child = edge[1] & 0xFF;
+            g[parent].children.push_back(child);
         }
 
-        topological_sort(1);
+        // 1 is always the root and all nodes are reachable
+        computeProfits(1);
 
-        return dfs(--topological_index, budget);
+        return std::ranges::max(g[1].regular);
     }
 };
