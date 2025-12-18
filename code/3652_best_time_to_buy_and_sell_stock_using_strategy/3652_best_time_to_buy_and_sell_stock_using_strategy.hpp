bad54e2b0cd92afa235e7bcfe12d4060ab1b0ff7
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2025-12-18 18:35:51 +0000

    Update

--- code/3652_best_time_to_buy_and_sell_stock_using_strategy/3652_best_time_to_buy_and_sell_stock_using_strategy.hpp
+++ code/3652_best_time_to_buy_and_sell_stock_using_strategy/3652_best_time_to_buy_and_sell_stock_using_strategy.hpp
@@ -15,25 +15,25 @@ public:
         // sk - profit sum over last k, original strategy
         for (u32 i = 0; i != k2; ++i)
         {
-            sk += i64{p[i]} * i64{s[i]};
+            sk += p[i] * s[i];
         }
 
         // sk - profit sum over last k, new strategy (sum of last k2)
         i64 sk2 = 0;
         for (u32 i = k2; i != k; ++i)
         {
-            sk += i64{p[i]} * i64{s[i]};
-            sk2 += i64{p[i]};
+            sk += p[i] * s[i];
+            sk2 += p[i];
         }
 
         i64 total = sk, d = sk2 - sk;
         for (u32 left = 0, mid = k2, right = k; right != n;
              ++left, ++mid, ++right)
         {
-            i64 ith_profit = i64{p[right]} * i64{s[right]};
-            total += ith_profit;
-            sk += ith_profit - i64{p[left]} * i64{s[left]};
-            sk2 += i64{p[right]} - i64{p[mid]};
+            int ip = p[right] * s[right];
+            total += ip;
+            sk += ip - p[left] * s[left];
+            sk2 += p[right] - p[mid];
             d = std::max(d, sk2 - sk);
         }
 
