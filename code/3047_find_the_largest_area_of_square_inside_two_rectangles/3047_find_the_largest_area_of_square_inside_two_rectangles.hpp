e13a06fe1f3f111532acfe0ee775d87e8bf6ea68
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2026-01-18 16:55:29 +0000

    Updates for 3047

--- code/3047_find_the_largest_area_of_square_inside_two_rectangles/3047_find_the_largest_area_of_square_inside_two_rectangles.hpp
+++ code/3047_find_the_largest_area_of_square_inside_two_rectangles/3047_find_the_largest_area_of_square_inside_two_rectangles.hpp
@@ -1,69 +1,45 @@
 #include <algorithm>
 #include <vector>
 
-#include "force_inline.hpp"
-
-struct Vec2
-{
-    int x, y;
-};
-
-struct Rect
-{
-    FORCE_INLINE constexpr int left_x() const noexcept { return bot_left.x; }
-    FORCE_INLINE constexpr int right_x() const noexcept { return top_right.x; }
-    Vec2 bot_left;
-    Vec2 top_right;
-};
-
-// Intersection between two intervals described as [x1, x2] and [x3, x4]
-template <std::integral T>
-[[nodiscard]] FORCE_INLINE constexpr int
-interval_intersection(T x1, T x2, T x3, T x4) noexcept
-{
-    return std::max(0, std::min(x2, x4) - std::max(x1, x3));
-}
-
 class Solution
 {
 public:
-    auto largestSquareArea(
+    using u32 = uint32_t;
+    [[nodiscard]] static constexpr auto largestSquareArea(
         const std::vector<std::vector<int>>& bottomLeft,
-        const std::vector<std::vector<int>>& topRight)
+        const std::vector<std::vector<int>>& topRight) noexcept
     {
-        size_t n = bottomLeft.size();
-
-        static Rect rects[1000];
-        for (size_t i = 0; i != n; ++i)
+        struct Rect
         {
-            rects[i] = {
-                .bot_left{.x = bottomLeft[i][0], .y = bottomLeft[i][1]},
-                .top_right{.x = topRight[i][0], .y = topRight[i][1]},
-            };
-        }
+            int x1, y1, x2, y2;
+            constexpr auto operator<=>(const Rect&) const noexcept = default;
+        };
 
-        std::ranges::sort(rects, rects + n, std::less{}, &Rect::left_x);
-
-        int ans = 0;
-        for (size_t i = 0; i != n; ++i)
+        const u32 n = static_cast<u32>(bottomLeft.size());
+        static Rect rects[1000];
+        for (u32 i = 0; i != n; ++i)
         {
             auto& ri = rects[i];
-            for (size_t j = i + 1; j != n; ++j)
+            ri.x1 = bottomLeft[i][0];
+            ri.y1 = bottomLeft[i][1];
+            ri.x2 = topRight[i][0];
+            ri.y2 = topRight[i][1];
+        }
+
+        std::ranges::sort(rects, rects + n);
+
+        int ans = 0;
+        for (u32 i = 0; i != n; ++i)
+        {
+            auto& ri = rects[i];
+            for (u32 j = i + 1; j != n; ++j)
             {
                 auto& rj = rects[j];
 
-                if (rj.left_x() > ri.right_x()) break;
+                if (rj.x1 > ri.x2) break;
 
-                int a = interval_intersection(
-                    ri.bot_left.x,
-                    ri.top_right.x,
-                    rj.bot_left.x,
-                    rj.top_right.x);
-                int b = interval_intersection(
-                    ri.bot_left.y,
-                    ri.top_right.y,
-                    rj.bot_left.y,
-                    rj.top_right.y);
+                int a = std::min(ri.x2, rj.x2) - rj.x1;
+                int b = std::min(ri.y2, rj.y2) - std::max(ri.y1, rj.y1);
                 ans = std::max(ans, std::min(a, b));
             }
         }
