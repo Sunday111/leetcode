7181de06c9d286f1e30ae09ea8490bca686a2cf6
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2026-02-02 18:56:46 +0000

    3013. Divide an Array Into Subarrays With Minimum Cost II

--- code/3013_divide_an_array_into_subarrays_with_minimum_cost_ii/3013_divide_an_array_into_subarrays_with_minimum_cost_ii_heaps_alloc.hpp
+++ code/3013_divide_an_array_into_subarrays_with_minimum_cost_ii/3013_divide_an_array_into_subarrays_with_minimum_cost_ii_heaps_alloc.hpp
@@ -1,122 +1,12 @@
 #include <algorithm>
 #include <cassert>
-#include <cstdint>
 #include <vector>
 
-#define FORCE_INLINE inline __attribute__((always_inline))
-#define INLINE_LAMBDA __attribute__((always_inline))
+#include "bump_allocator.hpp"
+#include "heap_with_erase.hpp"
+#include "integral_aliases.hpp"
 
-template <
-    typename T,
-    typename Cmp,
-    typename Proj = std::identity,
-    typename Allocator = std::allocator<T>>
-struct heap_with_erase
-{
-    template <typename Op>
-    FORCE_INLINE constexpr static void heap_op(
-        Op&& op,
-        std::vector<T>& rng) noexcept
-    {
-        std::forward<Op>(op)(rng, Cmp{});
-    }
-
-    FORCE_INLINE constexpr static void push_(std::vector<T>& rng, T v) noexcept
-    {
-        rng.push_back(v);
-        heap_op(std::ranges::push_heap, rng);
-    }
-
-    FORCE_INLINE constexpr static void pop_(std::vector<T>& rng) noexcept
-    {
-        heap_op(std::ranges::pop_heap, rng);
-        rng.pop_back();
-    }
-
-    FORCE_INLINE constexpr void reserve(size_t n) noexcept
-    {
-        queued.reserve(n);
-        erased.reserve(n);
-    }
-
-    template <typename It>
-    FORCE_INLINE constexpr void init(It begin, It end) noexcept
-    {
-        assert(empty());
-        queued.insert(queued.end(), begin, end);
-        heap_op(std::ranges::make_heap, queued);
-    }
-
-    [[nodiscard]] FORCE_INLINE const T& top() const noexcept
-    {
-        purge();
-        return queued.front();
-    }
-
-    FORCE_INLINE constexpr void pop() noexcept
-    {
-        purge();
-        pop_(queued);
-    }
-
-    [[nodiscard]] FORCE_INLINE constexpr bool empty() const noexcept
-    {
-        return queued.size() == erased.size();
-    }
-
-    [[nodiscard]] FORCE_INLINE constexpr size_t size() const noexcept
-    {
-        return queued.size() - erased.size();
-    }
-
-    FORCE_INLINE constexpr void push(const T& v) noexcept { push_(queued, v); }
-
-    FORCE_INLINE constexpr void erase(const T& v) noexcept { push_(erased, v); }
-
-private:
-    [[nodiscard]] FORCE_INLINE static constexpr bool eq(
-        const T& a,
-        const T& b) noexcept
-    {
-        constexpr Proj p{};
-        constexpr Cmp c{};
-        auto pa = p(a), pb = p(b);
-        return c(pa, pb) == c(pb, pa);
-    }
-
-    FORCE_INLINE constexpr void purge() const noexcept
-    {
-        while (!erased.empty() && eq(erased.front(), queued.front()))
-        {
-            pop_(erased);
-            pop_(queued);
-        }
-    }
-
-    mutable std::vector<T> queued, erased;
-};
-
-template <
-    typename T,
-    typename Proj = std::identity,
-    typename Allocator = std::allocator<T>>
-using min_heap_with_erase = heap_with_erase<T, std::greater<>, Proj, Allocator>;
-
-template <
-    typename T,
-    typename Proj = std::identity,
-    typename Allocator = std::allocator<T>>
-using max_heap_with_erase = heap_with_erase<T, std::less<>, Proj, Allocator>;
-
-using u8 = uint8_t;
-using u16 = uint16_t;
-using u32 = uint32_t;
-using u64 = uint64_t;
-
-using i8 = int8_t;
-using i16 = int16_t;
-using i32 = int32_t;
-using i64 = int64_t;
+using SolutionStorage = GlobalBufferStorage<1u << 25>;
 
 class Solution
 {
@@ -196,8 +86,16 @@ public:
             }
         }
 
-        max_heap_with_erase<u32> smaller;
-        min_heap_with_erase<u32> larger;
+        max_heap_with_erase<
+            u32,
+            std::identity,
+            BumpAllocator<u32, SolutionStorage>>
+            smaller;
+        min_heap_with_erase<
+            u32,
+            std::identity,
+            BumpAllocator<u32, SolutionStorage>>
+            larger;
         u64 sum = 0;
     };
 
@@ -205,7 +103,7 @@ public:
     minimumCost(const std::vector<u32>& nums, u32 k, u32 dist) const noexcept
     {
         const u32 n = static_cast<u32>(nums.size()), d2 = dist + 2;
-        // The special case when dist == k-2
+
         if (d2 == k)
         {
             u64 run_sum = 0;
