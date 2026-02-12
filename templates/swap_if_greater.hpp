43f2582fce3f03bba532b743cbc4a4ed01f022e7
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2026-02-12 08:03:08 +0000

    Use swap_if in swap_if_greater

--- templates/swap_if_greater.hpp
+++ templates/swap_if_greater.hpp
@@ -3,11 +3,11 @@
 #include <concepts>
 
 #include "force_inline.hpp"
+#include "swap_if.hpp"
 
 // Swaps a and b if a > b. Only for integers. Branchless
 template <std::integral Int>
 FORCE_INLINE static constexpr void swap_if_greater(Int& a, Int& b) noexcept
 {
-    Int mask = (a ^ b) & -Int{a > b};
-    a ^= mask, b ^= mask;
+    swap_if(a > b, a, b);
 }
