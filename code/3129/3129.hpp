4afd1e9d8ab2c6681c847b3dc51aad2266e93b73
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2026-03-09 01:21:13 +0000

    Update

--- code/3129/3129.hpp
+++ code/3129/3129.hpp
@@ -10,43 +10,35 @@ class Solution
     static constexpr i64 MOD = 1'000'000'007;
 
 public:
-    using SolutionStorage = GlobalBufferStorage<1 << 20>;
+    using SolutionStorage = GlobalBufferStorage<1 << 25>;
     template <typename T>
     using Vec = BumpVector<T, SolutionStorage>;
 
     i32 numberOfStableArrays(u16 zero, u16 one, u16 limit)
     {
         auto arena = SolutionStorage::Instance().StartArena();
+        u16 z1 = zero + 1, o1 = one + 1, l1 = limit + 1;
 
         Vec<Vec<std::array<i32, 2>>> dp;
-        dp.resize(zero + 1);
+        dp.resize(z1);
         for (auto& e : dp) e.resize(one + 1);
-
-        for (u16 i = std::min(zero, limit) + 1; i--;)
+        for (u16 i = std::min(z1, l1); i--;) dp[i][0][0] = 1;
+        for (u16 j = std::min(o1, l1); j--;) dp[0][j][1] = 1;
+        for (u16 i = 1; i != z1; ++i)
         {
-            dp[i][0][0] = 1;
-        }
-
-        for (u16 j = std::min(one, limit) + 1; j--;)
-        {
-            dp[0][j][1] = 1;
-        }
-
-        for (u16 i = 1; i <= zero; ++i)
-        {
-            for (u16 j = 1; j <= one; ++j)
+            for (u16 j = 1; j != o1; ++j)
             {
-                bool bi = i >= limit + 1;
-                bool bj = j >= limit + 1;
-                u16 loi = std::min<u16>(i - 1 - limit, zero);
-                u16 loj = std::min<u16>(j - 1 - limit, one);
+                u16 j1 = j - 1, i1 = i - 1;
+                i32 bi = i >= l1, bj = j >= l1;
+                u16 loi = std::min<u16>(i1 - limit, i1);
+                u16 loj = std::min<u16>(j1 - limit, j1);
                 dp[i][j][0] = static_cast<int>(
-                    (dp[i - 1][j][0] + dp[i - 1][j][1] -
-                     (dp[loi][j][1] & -i32{bi}) + MOD) %
+                    (dp[i1][j][0] + dp[i1][j][1] - (dp[loi][j][1] & -bi) +
+                     MOD) %
                     MOD);
                 dp[i][j][1] = static_cast<int>(
-                    (dp[i][j - 1][0] + dp[i][j - 1][1] -
-                     (dp[i][loj][0] & -i32{bj}) + MOD) %
+                    (dp[i][j1][0] + dp[i][j1][1] - (dp[i][loj][0] & -bj) +
+                     MOD) %
                     MOD);
             }
         }
