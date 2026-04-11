a3b4c182428b499fe623d8704ee1d21fcc69dae0
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2026-04-11 20:50:39 +0100

    3741. Minimum Distance Between Three Equal Elements II

---
+++ code/3741/3741.hpp
@@ -0,0 +1,24 @@
+#include <vector>
+
+class Solution
+{
+public:
+    using u32 = uint32_t;
+    int minimumDistance(std::vector<int>& nums_) noexcept
+    {
+        auto& nums = reinterpret_cast<const std::vector<u32>&>(nums_);
+        u32 n = static_cast<u32>(nums.size()), r = 2 * n;
+        std::tuple<u32, u32> a[100'001];
+        fill_n(a, n + 1, std::tuple{-n, -n});
+        for (u32 k = 0; k != n; ++k)
+        {
+            u32 v = nums[k];
+            [[assume(v < 100'001)]];
+            auto& [i, j] = a[v];
+            r = std::min(r, 2 * (k - i));
+            i = j;
+            j = k;
+        }
+        return r < (2 * n) ? static_cast<int>(r) : -1;
+    }
+};
---
+++ code/3741/3741_test_cases.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "3741.hpp"
+#include "test_cases_helpers.hpp"
+
+inline static constexpr auto kMethodToTest = &Solution::minimumDistance;
+inline static const auto kCases = parse_test_cases<kMethodToTest>(R"(
+[1,2,1,1,3]
+6
+
+[1,1,2,3,2,1,2]
+8
+
+[1]
+-1
+
+
+)");
