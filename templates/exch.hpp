da8ec13b87bd573765ff88cc0e73e9a42ea64a36
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2026-05-28 15:03:22 +0000

    Update exch.hpp

--- templates/exch.hpp
+++ templates/exch.hpp
@@ -5,7 +5,9 @@
 // simplified std::exchange
 template <typename T>
     requires std::is_trivially_copyable_v<T>
-[[nodiscard, gnu::always_inline]] constexpr T exch(T& x, T new_value) noexcept
+[[nodiscard, gnu::always_inline]] constexpr T exch(
+    T& x,
+    std::type_identity_t<T> new_value) noexcept
 {
     T tmp = x;
     x = new_value;
