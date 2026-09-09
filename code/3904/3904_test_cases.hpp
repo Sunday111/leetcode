7d273150b5ff452613654b9015abe5061128bfcd
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2026-09-09 19:54:37 +0100

    3904

---
+++ code/3904/3904_test_cases.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "3904.hpp"
+#include "test_cases_helpers.hpp"
+
+inline static constexpr auto kMethodToTest = &Solution::firstStableIndex;
+inline static const auto kCases = parse_test_cases<kMethodToTest>(R"(
+[5,0,1,4]
+3
+3
+
+[3,2,1]
+1
+-1
+
+[0]
+0
+0
+
+
+)");
