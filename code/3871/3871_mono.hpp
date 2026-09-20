bf7bd47b88406f0982220291f90431b87bd26273
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2026-09-20 19:01:00 +0100

    3871

---
+++ code/3871/3871_mono.hpp
@@ -0,0 +1,17 @@
+
+class Solution
+{
+public:
+    auto countCommas(unsigned long n)
+    {
+        auto r = 0ul, t = 1000ul;
+        n += 1;
+        while (n > t)
+        {
+            r += n - t;
+            t *= 1000ul;
+        }
+
+        return r;
+    }
+};
